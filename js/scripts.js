c5a899f70e834ba66099981e72fe7ef21f0a7a3d
Jamey Smith <smith.jameyd@gmail.com>
2020-12-30 19:22:31 -0800

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(function () {
+  $(".carousel").carousel({ interval: 2000, pause: "false" });
+  // toggle carousel button class on click
+  $("#carouselButton").click(function () {
+    // on click check if carousel button is set to pause using icon class
+    //if paused, run carousel pause method, remove pause class, apply play class
+    if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+      $(".carousel").carousel("pause");
+      $("#carouselButton").children("i").removeClass("fa-pause");
+      $("#carouselButton").children("i").addClass("fa-play");
+      //on click if class is set to play, run carousel cycle method, remove play class, add pause class
+    } else {
+      $(".carousel").carousel("cycle");
+      $("#carouselButton").children("i").removeClass("fa-play");
+      $("#carouselButton").children("i").addClass("fa-pause");
+    }
+  });
+});
